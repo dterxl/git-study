158960c1ebd49d7e0c7b01a5310db058b8e08fe4
dterxl <760132419@qq.com>
2022-04-16 12:49:13 +0800

    hot-fox first commit

--- git01/src/main/java/com/dterxl/GitTest.java
+++ git01/src/main/java/com/dterxl/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试idea提交git");
         System.out.println("第二次提交");
+        System.out.println("热修复1");
     }
 }
